b7da8896e0bb9d3af6c0c136b1eb0a270ab315e7
Kate Roland <kate.roland1@outlook.com>
2024-07-28 16:17:17 -0400

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import styles from "../Footer/Footer.module.css";
 function Footer() {
-  return <div className={styles.footer}>Footer</div>;
+  return (
+    <div className={styles.footer}>
+      <h1 className={styles.classInfo}>C856 - UI/UX Design - Task 1</h1>
+    </div>
+  );
 }
 
 export default Footer;
